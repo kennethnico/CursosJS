934ac14829e38c6d4b240643239ca6274987a3e3
parseedval <krenicgm@gmail.com>
2022-05-25 13:56:33 -0500

    Clase Arrays

--- Intro_JS/arrays.js
+++ Intro_JS/arrays.js
@@ -1,9 +1,11 @@
 let frutas =["Fresa","Manzana","Piña","Banana"];
 console.log(frutas.length) //Tamaño del arreglo
 console.log(frutas);
-frutas.push("Kiwi"); //Inserta un elemento hasta el final del arreglo.
+frutas.push("Kiwi","Uva"); //Inserta un elemento hasta el final del arreglo.
 console.log(frutas);
 frutas.pop(); //Elimina el último elemento del arreglo.
+console.log(frutas);
+frutas.unshift("Frutotas"); //Agrega un elemento al inicio del Array.
 console.log(frutas);
 
 console.log("Enlistemos a las frutas:");
