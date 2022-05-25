427df02e511b543cc4d8f44636020e39a42a30c9
parseedval <krenicgm@gmail.com>
2022-05-25 11:23:03 -0500

    Hoisting de Funciones

---
+++ Intro_JS/hoisting.js
@@ -0,0 +1,19 @@
+console.log(miNombre);
+miNombre = "Edwin Kenet";
+//undefined
+
+let myName = undefined;
+console.log(myName);
+myName="Edwin Kenet";
+// undefined
+// Edwin Kenet
+
+/**Hoisting de funciones**/
+hey(); //Mandamos a llamar la función.
+function hey(){ //Declaramos la función.
+    console.log("Hola compañero " + myNName); //¿Qué imprime?
+}
+let myNName ="Edwin";
+// Hola compañero undefined
+// Las funciones se procesan antes de ejecutar cualquier código
+//Pero las variables se declaran después.
