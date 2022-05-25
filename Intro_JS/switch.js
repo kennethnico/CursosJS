b815d95664e11f5e7108bc90e5f0d53eb7214ac7
parseedval <krenicgm@gmail.com>
2022-05-25 13:39:48 -0500

    Clase Switch

---
+++ Intro_JS/switch.js
@@ -0,0 +1,12 @@
+let num =1;
+switch(num){
+    case 1:
+        console.log("Soy 1");
+        break;
+    case 2:
+        console.log("Soy 2");
+        break;
+    default:
+        console.log("Caso default");
+        break;
+}
