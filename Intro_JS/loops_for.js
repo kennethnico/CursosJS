adb55174b9f3d94e1b0db537bdb586f5c2fe30b1
parseedval <krenicgm@gmail.com>
2022-05-25 14:14:36 -0500

    Clase Loops - For

---
+++ Intro_JS/loops_for.js
@@ -0,0 +1,14 @@
+/*
+* for - recorre un bloque de código varias veces
+* for/in - recorre las propiedades de un objeto
+* for/of - recorre los valores de un objeto iterable
+* while - recorre un bloque de código mientras se cumple una condición específica
+* do/while - también recorre un bloque de código mientras se cumple una condición específica*/
+let estudiantes =["Edwin", "Jamy", "Ferit", "Panchis"];
+
+function saludarE(estudiante){
+    console.log(`Hola, ${estudiante}`);
+}
+for(let i=0;i<estudiantes.length;i++){
+    saludarE(estudiantes[i]);
+}
