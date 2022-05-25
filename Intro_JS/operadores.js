d9113b3a6af745cde72e9321e8ed56d100027536
parseedval <krenicgm@gmail.com>
2022-05-25 12:12:00 -0500

    Clase Operadores: Asignación Comparación y Aritméticos

---
+++ Intro_JS/operadores.js
@@ -0,0 +1,13 @@
+/*Operadores: Asignación, Comparación y Aritméticos*/
+
+/*Operación
+* Operadores Binarios*/
+let a = 3 + 5; //Suma
+let b = 3 - 5; //Resta
+let c = 3 * 5; //Multiplicación
+let d = 3 / 5; //División
+
+let e = "Kenet" + "Nicolas Ruiz"; //Concatena con el operador +
+let f = !false; //Negación
+
+let g = a; //Operador de asignación
