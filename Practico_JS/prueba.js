560f52538761126b512e3ba42041b61380656e43
Edwin Kenet <krenicgm@gmail.com>
2022-05-30 10:46:03 -0500

    Curso JS Práctico - Condicionales 30

--- Practico_JS/prueba.js
+++ Practico_JS/prueba.js
@@ -14,7 +14,50 @@ console.log("Dinero real : $"+String(estudiante.dinero_ahorrado-estudiante.deuda
 
 //Funciones
 function miFuncion(nombre, apellido, nickname){
-    console.log("Mi nombre es: "+nombre+" "+apellido+", pero prefiero que me digas: "+nickname);
+    return "Mi nombre es: "+nombre+" "+apellido+", pero prefiero que me digas: "+nickname;
 }
 
 console.log(miFuncion("Edwin Kenet","Nicolás", "Ken"));
+
+//Condicionales
+const tipoDeSuscripcion0 = "Basic";
+switch (tipoDeSuscripcion0) {
+    case "Free":
+        console.log("Solo puedes tomar los cursos gratis");
+        break;
+    case "Basic":
+        console.log("Puedes tomar casi todos los cursos de Platzi durante un mes");
+        break;
+    case "Expert":
+        console.log("Puedes tomar casi todos los cursos de Platzi durante un año");
+        break;
+    case "ExpertPlus":
+        console.log("Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año");
+        break;
+}
+
+   /*Usando If, else if & else*/
+const tipoDeSuscripcion1 = "Expert";
+if(tipoDeSuscripcion1 === "Free"){
+    console.log("Solo puedes tomar los cursos gratis");
+}else if(tipoDeSuscripcion1 === "Basic"){
+    console.log("Puedes tomar casi todos los cursos de Platzi durante un mes");
+}else if(tipoDeSuscripcion1 === "Expert"){
+    console.log("Puedes tomar casi todos los cursos de Platzi durante un año");
+}else{
+    console.log("Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año");
+}
+    /*Usando solo if*/
+const tipoDeSuscripcion2 = "ExpertPlus";
+let respuesta = tipoDeSuscripcion2 === "Free" ? "Solo puedes tomar los cursos gratis" : tipoDeSuscripcion2 === "Basic" ? "Puedes tomar casi todos los cursos de Platzi durante un mes": tipoDeSuscripcion2 === "Expert" ? "Puedes tomar casi todos los cursos de Platzi durante un año" : "Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año";
+
+ /*Usando arrays y solo un condicional */
+let suscripcion ="Free";
+let tipoDeSuscripcion3 = ["Free", "Basic","Expert", "ExpertPlus"];
+let mensaje = ["Solo puedes tomar los cursos gratis","Puedes tomar casi todos los cursos de Platzi durante un mes","Puedes tomar casi todos los cursos de Platzi durante un año","Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año"];
+if (tipoDeSuscripcion3.indexOf(suscripcion) !== -1){
+    console.log(mensaje[tipoDeSuscripcion3.indexOf(suscripcion)]);
+}
+
+
+
