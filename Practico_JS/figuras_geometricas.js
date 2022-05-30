ddfa567089fe62af9aa23d2e8638b4f8050500f8
Edwin Kenet <krenicgm@gmail.com>
2022-05-30 13:24:06 -0500

    Curso JS Práctico - Figuras Geométricas - Primeras operaciones definidas

--- Practico_JS/figuras_geometricas.js
+++ Practico_JS/figuras_geometricas.js
@@ -1,1 +1,8 @@
 /*Funciones de figuras.html*/
+//Cuadrado
+const ladoCuadrado = 5; //Medida en centimetros.
+const perimetroCuadrado = ladoCuadrado*4;
+const areaCuadrado = ladoCuadrado*ladoCuadrado;
+console.log("Los lados del cuadrado miden: "+ladoCuadrado+" cms." );
+console.log("El perimetro del cuadrado es: "+perimetroCuadrado+" cms." );
+console.log("El área del cuadrado es: "+areaCuadrado+" cms2." );
