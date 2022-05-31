93473d7ed4ba46dcabdf624c498874af65b032cd
Edwin Kenet <krenicgm@gmail.com>
2022-05-31 14:02:55 -0500

    Curso JS Práctico - Taller 2

--- Practico_JS/taller2_descuentos.js
+++ Practico_JS/taller2_descuentos.js
@@ -1,8 +1,15 @@
 //Formula para descuentos p * (100 - descuento)/100
-let precio;
-let descuento;
+/*let precio = document.getElementById("precioProducto").value;
+let descuento =document.getElementById("descuentoProducto").value;*/
 let costoFinal;
 
-function precioOferta(precio,descuento){
-    return (precio * (100-descuento))/100;
+function precioOferta(){
+    let precio = document.getElementById("precioProducto").value;
+    let descuento =document.getElementById("descuentoProducto").value;
+    costoFinal = (precio * (100-descuento))/100;
+    console.log(costoFinal);
+    document.getElementById("resultado_del_descuento").innerText = "El precio con descuento es: $"+costoFinal;
 }
+
+//costoFinal = precioOferta(precio,descuento);
+//document.getElementById("resultado_del_descuento").innerText = costoFinal;
