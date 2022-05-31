a39f35d47bd78ad6b9fc651c0606114c360ab48f
Edwin Kenet <krenicgm@gmail.com>
2022-05-31 10:19:42 -0500

    Curso JS Práctico - Reto del taller 1

--- Practico_JS/taller1_reto.js
+++ Practico_JS/taller1_reto.js
@@ -1,7 +1,12 @@
 function calculaAltura(ladoA, ladoB, base){
     if(ladoA !== ladoB){
-        console.log("Las medidas dadas no corresponden a un triángulo isoseles");
+        console.log("Las medidas dadas no corresponden a un triángulo isosceles");
     }else{
-
+        //Dividimos en a la mitad el triángulo, y sus lados serían a=base/2, c=ladoA b=altura a calcular
+        //c^2 = a^2 + b^2 => b^2 = c^2 - a^2
+        let a= base/2;
+        let c = ladoA;
+        return Math.sqrt((c*c)-(a*a));
     }
 }
+calculaAltura(5,5,6);
