ebc4a716eeb28f26db782fd5d022e4417779f227
kenethnic <79937993+kennethnico@users.noreply.github.com>
2022-08-03 11:24:15 -0500

    ES8 - 1 parte

--- EcmaScript/ES8/index.js
+++ EcmaScript/ES8/index.js
@@ -1,4 +1,6 @@
 //Liberado en Junio de 2017
+
+//Herramientas para convertir objetos a arreglos/matrices
 const data = {
     frontend:'Oscar',
     backend:'Isabel',
@@ -6,3 +8,23 @@ const data = {
 };
 const entries = Object.entries(data);
 console.log(entries);
+console.log(entries.length)
+
+//Object Values - Valores de un objeto a un arreglo
+const data2 = {
+    frontend:'Oscar',
+    backend:'Isabel',
+    design:'Ana'
+};
+const values = Object.values(data2);
+console.log(values);
+
+
+//Padding
+const string = 'hello';
+console.log(string.padStart(7,'*** '));
+console.log(string.padEnd(12,' -------'));
+
+const obj ={
+    name: 'Edwin',
+}
