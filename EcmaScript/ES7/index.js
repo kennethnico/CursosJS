9c5cdc1e7dbadc496cf517a60c91ab8fb8ee27cc
kenethnic <79937993+kennethnico@users.noreply.github.com>
2022-08-03 10:58:02 -0500

    ES8

---
+++ EcmaScript/ES7/index.js
@@ -0,0 +1,13 @@
+let numbers = [1,2,3,7,8];
+//Buscar un valor en el arreglo
+if (numbers.includes(7)){
+    console.log("Si se encuentre el valor 7");
+}else{
+    console.log("No se encuentra")
+}
+
+//Potencia
+let base = 4;
+let exponente = 3;
+let result = base ** exponente;
+console.log(result);
---
+++ EcmaScript/ES8/index.js
@@ -0,0 +1,8 @@
+//Liberado en Junio de 2017
+const data = {
+    frontend:'Oscar',
+    backend:'Isabel',
+    design:'Ana'
+};
+const entries = Object.entries(data);
+console.log(entries);
