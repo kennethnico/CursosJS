49e1147d8803dd1a13be50dcf471dca5366b64f4
kenethnic <79937993+kennethnico@users.noreply.github.com>
2022-08-03 11:36:27 -0500

    ES8 - 2 parte

--- EcmaScript/ES8/index.js
+++ EcmaScript/ES8/index.js
@@ -28,3 +28,29 @@ console.log(string.padEnd(12,' -------'));
 const obj ={
     name: 'Edwin',
 }
+
+//Async y Await
+//Promesas
+const helloworld = () => {
+    return new Promise((resolve, reject) => {
+        (0)
+            ? setTimeout(() => resolve('Hello World'), 3000)
+            : reject(new Error('Test Error'));
+    })
+};
+
+const helloAsync = async () =>{
+    const hello = await helloworld();
+    console.log(hello);
+};
+helloAsync();
+
+const anotherFunction = async () => {
+    try{
+        const hello = await helloworld();
+        console.log(hello);
+    }catch (error){
+        console.log(error);
+    }
+};
+anotherFunction();
