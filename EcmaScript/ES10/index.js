579fde12384cae1386af39241203960b5992a05c
kenethnic <79937993+kennethnico@users.noreply.github.com>
2022-08-08 11:36:50 -0500

    ES10 - Flat/FlatMap/Trim/Optiona catch/fromEntries/Symbol

--- EcmaScript/ES10/index.js
+++ EcmaScript/ES10/index.js
@@ -1,3 +1,35 @@
 //Aplanar arreglos
 let array =[1,2,3, [1,2,3, [1,2,3]]];
-console.log(array.flat());
+console.log(array.flat(2));
+
+//Flat Map
+let array2 =[1,2,3,4,5];
+console.log(array2.flatMap(value => [value, value*2]));
+
+//Trim Start - Elimina espacios en blanco de string
+let hello= "        Hello world        ";
+console.log(hello);
+console.log(hello.trimStart());
+console.log(hello.trimEnd());
+
+//Optional catch binding - Pasar de forma opcional el Parametro de error al Catch
+/*
+Antes
+try {
+}catch(error){
+error
+}
+Ahora
+try{
+}catch{
+error
+}
+* */
+
+//Construir un objeto a partir de un arreglo
+let entries = [["name","Edwin"],["age",29]];
+console.log(Object.fromEntries(entries));
+
+let mySymbol = `My symbol`;
+symbol = Symbol(mySymbol);
+console.log(symbol.description);
